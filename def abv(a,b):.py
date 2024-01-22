11e65b4ca7b487df45ce96ac051a78f20fb0d5a6
HENRI <gouabeedi@gmail.com>
2024-01-22 11:36:07 +0100

    deuxieme methode

--- def abv(a,b):.py
+++
@@ -1,2 +0,0 @@
-def abv(a,b):
-    return a+b
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import datetime
+
+import pytz
+
+#methode 1
 heure_actuelle =datetime.datetime.now().strftime(("%H:%M:%S"))
-print("L'heure actuelle est :", heure_actuelle)
+#print("L'heure actuelle est :", heure_actuelle)
+
+#methode2
+
+    # obtention de la date et l'heure actuelle
+heure_locale =datetime.datetime.now()
+    #definition du fuseau horaire cible
+fuseau_cible=pytz.timezone("Europe/Paris")
+    #application du zonage sur la date et l'heure
+heure_cible=heure_locale.astimezone(fuseau_cible)
+    #extraire l'heure dans le fuseau horaire cible
+heure= heure_cible.strftime("%H:%M:%S")
+print("L'heure actuelle dans le fuseau horaire est :", heure)
+
+
+    
